0e8b73dfb6b3f7bc390e72698087b147a7bff737
aadikulkarni91 <aadikulkarni91@gmail.com>
2016-04-05 00:13:04 -0500

    Comments for admin and advisor

--- src/uta/mav/appoint/db/command/UpdateAppointment.java
+++ src/uta/mav/appoint/db/command/UpdateAppointment.java
@@ -10,20 +10,25 @@ public class UpdateAppointment extends SQLCmd{
 	String studentid;
 	int id;
 	Boolean b = false;
+	String comments;
 	
 	public UpdateAppointment(Appointment a){
 		description = a.getDescription();
 		studentid = a.getStudentId();
 		id = a.getAppointmentId();
+		comments = a.getAdvisorComments();
 	}
 	
 	public void queryDB(){
 		try{
-			String command = "UPDATE appointments SET description=?,studentId=? where id=?";
+			String command = "UPDATE appointments SET description=?,studentId=?,comments=? where id=?";
 			PreparedStatement statement = conn.prepareStatement(command);
 			statement.setString(1, description);
 			statement.setString(2, studentid);
-			statement.setInt(3, id);
+			statement.setString(3, comments);
+			statement.setInt(4, id);
+			
+			
 			statement.executeUpdate();
 			b=true;
 		}
